0e81be6dc9b186c1c1044fe69f326835429aba0a
Mehran Khaksar <itsaboutmehran@yahoo.com>
2022-05-08 13:17:13 +0430

    some changes

--- redux/store.ts
+++ redux/store.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux';
 import thunk from 'redux-thunk';
+import { useSelector, TypedUseSelectorHook, useDispatch } from 'react-redux';
 import rootReducer from './rootReducer';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
@@ -8,7 +8,7 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 export type AppState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
+export const useAppDispatch = (): AppDispatch => useDispatch<AppDispatch>();
 
 export default store;
